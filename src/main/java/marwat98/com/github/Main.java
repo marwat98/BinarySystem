408c5304aee5727875eb2cecbe96d0b7b2da49ba
Bartek <szczotka.bartlomiej@gmail.com>
2022-05-03 10:48:14 +0200

    update console message

--- src/main/java/marwat98/com/github/Main.java
+++ src/main/java/marwat98/com/github/Main.java
@@ -3,5 +3,7 @@ package marwat98.com.github;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("Hi Marcin");
     }
+
 }
