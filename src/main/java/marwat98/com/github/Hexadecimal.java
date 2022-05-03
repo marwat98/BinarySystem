bc42f8ba394ec0660483614b618923d440ecd0bf
marcw <mw.981@wp.pl>
2022-05-03 23:03:11 +0200

    Binary system

---
+++ src/main/java/marwat98/com/github/Hexadecimal.java
@@ -0,0 +1,18 @@
+package marwat98.com.github;
+import java.util.Scanner;
+
+public class Hexadecimal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give me a number");
+        int number = scanner.nextInt();
+        for (int i = 15; i >= 0; i--){
+            if((number & 0b1 << i) != 0b0)
+                System.out.print("1");
+            else
+                System.out.print("0");
+        }
+
+    }
+}
+
