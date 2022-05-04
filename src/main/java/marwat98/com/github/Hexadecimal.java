c023c579778542b295ed6f602e92249e171f19c5
marcw <mw.981@wp.pl>
2022-05-04 11:38:21 +0200

    Binary system 2

--- src/main/java/marwat98/com/github/Hexadecimal.java
+++ src/main/java/marwat98/com/github/Hexadecimal.java
@@ -1,18 +1,36 @@
 package marwat98.com.github;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
+
+
 
 public class Hexadecimal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give me a number");
-        int number = scanner.nextInt();
-        for (int i = 15; i >= 0; i--){
-            if((number & 0b1 << i) != 0b0)
-                System.out.print("1");
-            else
-                System.out.print("0");
-        }
+        LinkedHashMap<String, > numbers = new LinkedHashMap<>();
+        numbers.put("0","0000");
+        numbers.put("1","0001");
+        numbers.put("2","0010");
+        numbers.put("3","0011");
+        numbers.put("4","0100");
+        numbers.put("5","0101");
+        numbers.put("6","0110");
+        numbers.put("7","0111");
+        numbers.put("8","1000");
+        numbers.put("9","1001");
+        numbers.put("A","1010");
+        numbers.put("B","1011");
+        numbers.put("C","1100");
+        numbers.put("D","1101");
+        numbers.put("E","1110");
+        numbers.put("F","1111");
+        String LinkedHashMap = scanner.next();
+        System.out.println(LinkedHashMap);
 
+
+        }
     }
-}
+
+
 
