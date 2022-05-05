b6112a39da86a0ecc3d628a73498e3ca60e82aba
marcw <mw.981@wp.pl>
2022-05-05 12:14:26 +0200

    Binary system 4

--- src/main/java/marwat98/com/github/Hexadecimal.java
+++ src/main/java/marwat98/com/github/Hexadecimal.java
@@ -7,18 +7,15 @@ public class Hexadecimal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give me a number");
-        double x = scanner.nextDouble();
-        for (int i = 0; i <= x; i++) {
-            if(x % 2 ==0){
-                x = i;
-                i++;
-            }
-            System.out.println(x/2);
-            break;
-            
-        }
+        int a = scanner.nextInt();
+        double b = scanner.nextDouble();
+        if (a == b) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
         }
     }
+}
 
 
 
