897b8c501e0d2d2b81c7d2728c783d3bfb2ebcd4
marcw <mw.981@wp.pl>
2022-05-06 11:38:55 +0200

    Binary and hex

---
+++ src/main/java/marwat98/com/github/Binary.java
@@ -0,0 +1,25 @@
+package marwat98.com.github;
+import java.util.Scanner;
+
+
+
+public class Binary {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give me a number");
+       int a = scanner.nextInt();
+        for (int i = 15; i >= 0; i--) {
+            if((a & 0b1 << i) != 0b0 ){
+                System.out.print("1");
+            }
+            else
+                System.out.print("0");
+        }
+    }
+}
+
+
+
+
+
--- src/main/java/marwat98/com/github/Hexadecimal.java
+++ src/main/java/marwat98/com/github/Hexadecimal.java
@@ -1,25 +1,28 @@
 package marwat98.com.github;
 import java.util.Scanner;
-
-
+import java.util.LinkedHashMap;
 
 public class Hexadecimal {
     public static void main(String[] args) {
-
+        LinkedHashMap<String , String> ten = new LinkedHashMap<>();
+        ten.put("0000", "0");
+        ten.put("0001", "1");
+        ten.put("0010", "2");
+        ten.put("0011", "3");
+        ten.put("0100", "4");
+        ten.put("0101", "5");
+        ten.put("0110", "6");
+        ten.put("0111", "7");
+        ten.put("1000", "8");
+        ten.put("1001", "9");
+        ten.put("1010", "A");
+        ten.put("1011", "B");
+        ten.put("1100", "C");
+        ten.put("1101", "D");
+        ten.put("1110", "E");
+        ten.put("1111", "F");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Give me a number");
-       int a = scanner.nextInt();
-        for (int i = 15; i >= 0; i--) {
-            if((a & 0b1 << i) != 0b0 ){
-                System.out.print("1");
-            }
-            else
-                System.out.print("0");
-        }
+        int x = scanner.nextInt();
     }
+
 }
-
-
-
-
-
